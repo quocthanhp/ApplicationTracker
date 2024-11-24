5e28a0cd7193b76defa2ada91ede091482cd0eeb
quocthanhp <phamquocthanh1502@gmail.com>
2024-11-24 21:45:58 +1100

    Add Login

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db7599efc0fc5924d82a6ece571da787647d887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeaf52610e57bb72cd9214d604808b00e0e023a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
